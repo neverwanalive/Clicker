9f0b456571a43963ef62392e443be8ed3ac789ef
Anton Avsioukov <anton.avsioukov@gmail.com>
2023-01-22 04:05:10 +0300

    renamed items

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,59 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import { itemsArr } from '../../items/items';
-import { saveGame, loadGame } from '../../saves/saves';
-import { Main, MainContainer, PointsTittle, AddButton, ItemsContainer, Item } from './App.styles';
+import React, { useState, useEffect } from "react";
+import { itemsArr } from "../../items/items";
+import { saveGame, loadGame } from "../../saves/saves";
+import {
+  Main,
+  MainContainer,
+  PointsTittle,
+  AddButton,
+  ItemsContainer,
+  Item,
+} from "./App.styles";
 
 export const App = () => {
-    const [points, updatePoints] = useState(0)
-    const [pointsPerClick, updatePointsPerClick] = useState(1)
-    const [pointsPerSecond, updatePointsPerSecond] = useState(1)
+  const [points, updatePoints] = useState(0);
+  const [pointsPerClick, updatePointsPerClick] = useState(1);
+  const [pointsPerSecond, updatePointsPerSecond] = useState(1);
 
-    const buyItem = (item) => {
-        if (points >= item.price && item.amount < 10) {
-            item.type === 'Бафф за клик'?
-            updatePointsPerClick(pointsPerClick + item.buffPerClick):
-            updatePointsPerSecond(pointsPerSecond + item.buffPerSecond)
-            updatePoints(points - item.price)
-            item.price = Math.round(item.price * 1.5)
-            item.amount = item.amount + 1
-        }
+  const buyItem = (item) => {
+    if (points >= item.price && item.amount < 10) {
+      item.type === "Points per click"
+        ? updatePointsPerClick(pointsPerClick + item.buffPerClick)
+        : updatePointsPerSecond(pointsPerSecond + item.buffPerSecond);
+      updatePoints(points - item.price);
+      item.price = Math.round(item.price * 1.5);
+      item.amount = item.amount + 1;
     }
+  };
 
-    useEffect(() => {
-        loadGame(updatePoints, updatePointsPerSecond, updatePointsPerClick)
-    }, []);
+  useEffect(() => {
+    loadGame(updatePoints, updatePointsPerSecond, updatePointsPerClick);
+  }, []);
 
-    useEffect(() => {
-        saveGame(points, pointsPerSecond, pointsPerClick)
-    }, [points, pointsPerSecond, pointsPerClick]);
+  useEffect(() => {
+    saveGame(points, pointsPerSecond, pointsPerClick);
+  }, [points, pointsPerSecond, pointsPerClick]);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            updatePoints((currPoints) => currPoints + pointsPerSecond)
-        }, 1000);
-        
-        return () => clearInterval(interval)
-    }, [pointsPerSecond]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updatePoints((currPoints) => currPoints + pointsPerSecond);
+    }, 1000);
 
-    return (
-        <Main>
-            <MainContainer>
-                <PointsTittle>Длина члена: {points}</PointsTittle>
-                <AddButton onClick={()=> updatePoints(points + pointsPerClick)}>Увеличить</AddButton>
-            </MainContainer>
-            <ItemsContainer>
-                {itemsArr.map(item => 
-                    <Item onClick={() => buyItem(item)}>
-                        {item.name}<br/>
-                        {item.type}: {item.type === 'Бафф за клик'?
-                        item.buffPerClick:
-                        item.buffPerSecond
-                        }<br/>
-                        Количество: {item.amount}<br/>
-                        Цена: {item.amount === 10 ? 'Зачилься' : item.price}
-                    </Item>
-                )}
-            </ItemsContainer>
-        </Main>
-    );
-}
+    return () => clearInterval(interval);
+  }, [pointsPerSecond]);
+
+  return (
+    <Main>
+      <MainContainer>
+        <PointsTittle>Total points: {points}</PointsTittle>
+        <AddButton onClick={() => updatePoints(points + pointsPerClick)}>
+          Click
+        </AddButton>
+      </MainContainer>
+      <ItemsContainer>
+        {itemsArr.map((item) => (
+          <Item onClick={() => buyItem(item)}>
+            {item.name}
+            <br />
+            {item.type}:{" "}
+            {item.type === "Points per click"
+              ? item.buffPerClick
+              : item.buffPerSecond}
+            <br />
+            Amount: {item.amount}
+            <br />
+            Price: {item.amount === 10 ? "MAX" : item.price}
+          </Item>
+        ))}
+      </ItemsContainer>
+    </Main>
+  );
+};
--- src/items/items.js
+++ src/items/items.js
@@ -1,60 +1,60 @@
 export const itemsArr = [
-    {
-        type: 'Бафф за клик',
-        name: 'Энергетик',
-        amount: 0,
-        price: 100,
-        buffPerClick: 1
-    },
-    {
-        type: 'Бафф за клик',
-        name: 'Пивко',
-        amount: 0,
-        price: 500,
-        buffPerClick: 10
-    }, 
-    {
-        type: 'Бафф за клик',
-        name: 'Вискарик',
-        amount: 0,
-        price: 2500,
-        buffPerClick: 100
-    }, 
-    {
-        type: 'Бафф за клик',
-        name: 'Ведро бухла',
-        amount: 0,
-        price: 5000,
-        buffPerClick: 200
-    },  
-    {
-        type: 'Бафф в секунду',
-        name: 'Хлебушек',
-        amount: 0,
-        price: 100,
-        buffPerClick: 0,
-        buffPerSecond: 1
-    },  
-    {
-        type: 'Бафф в секунду',
-        name: 'Чипсики',
-        amount: 0,
-        price: 500,
-        buffPerClick: 0,
-        buffPerSecond: 10,
-    }, 
-    {
-        type: 'Бафф в секунду',
-        name: 'Омарчик',
-        amount: 0,
-        price: 2500,
-        buffPerSecond: 100,
-    }, 
-    {
-        type: 'Бафф в секунду',
-        name: 'Ведро икорки',
-        amount: 0,
-        price: 5000,
-        buffPerSecond: 200,
-    }
-]
+  {
+    type: "Points per click",
+    name: "PPC 1",
+    amount: 0,
+    price: 100,
+    buffPerClick: 1,
+  },
+  {
+    type: "Points per click",
+    name: "PPC 2",
+    amount: 0,
+    price: 500,
+    buffPerClick: 10,
+  },
+  {
+    type: "Points per click",
+    name: "PPC 3",
+    amount: 0,
+    price: 2500,
+    buffPerClick: 100,
+  },
+  {
+    type: "Points per click",
+    name: "PPC 4",
+    amount: 0,
+    price: 5000,
+    buffPerClick: 200,
+  },
+  {
+    type: "Points per second",
+    name: "PPS 1",
+    amount: 0,
+    price: 100,
+    buffPerClick: 0,
+    buffPerSecond: 1,
+  },
+  {
+    type: "Points per second",
+    name: "PPS 2",
+    amount: 0,
+    price: 500,
+    buffPerClick: 0,
+    buffPerSecond: 10,
+  },
+  {
+    type: "Points per second",
+    name: "PPS 3",
+    amount: 0,
+    price: 2500,
+    buffPerSecond: 100,
+  },
+  {
+    type: "Points per second",
+    name: "PPS 4",
+    amount: 0,
+    price: 5000,
+    buffPerSecond: 200,
+  },
+];
